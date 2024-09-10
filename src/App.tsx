1e2e39f0645a8fb7450a42950e832175a9a8c733
bakNa2t <ok2.lock@gmail.com>
2024-09-10 10:42:50 +0300

    Updated App by adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from "react-router-dom";
+
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import { Home } from "./_root/pages";
+import "./index.css";
+import RootLayout from "./_root/RootLayout";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* Private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
