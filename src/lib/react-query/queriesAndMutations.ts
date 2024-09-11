6dc29c741d84ff5ae85bbcc48b86a6d96e7451c9
bakNa2t <ok2.lock@gmail.com>
2024-09-11 16:08:58 +0300

    Added tanstack queries and mutations

---
+++ src/lib/react-query/queriesAndMutations.ts
@@ -0,0 +1,21 @@
+import {
+  //   useQuery,
+  useMutation,
+  //   useQueryClient,
+  //   useInfiniteQuery,
+} from "@tanstack/react-query";
+import { createUserAccount, signInAccount } from "../appwrite/api";
+import { INewUser } from "@/types";
+
+export const useCreateAccountMutation = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSignInAccountMutation = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
