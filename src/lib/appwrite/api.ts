9915aa3ac1a8e6c56f97dc3f51b245846413bf19
bakNa2t <ok2.lock@gmail.com>
2024-09-12 10:26:52 +0300

    Added getCurrentUser fn

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { ID } from "appwrite";
+import { ID, Query } from "appwrite";
 import { INewUser } from "@/types";
 import { account, appwriteConfig, avatars, databases } from "./config";
 
@@ -59,6 +59,26 @@ export async function signInAccount(user: { email: string; password: string }) {
     );
 
     return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await account.get();
+
+    if (!currentAccount) throw new Error();
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw new Error();
+
+    return currentUser.documents[0];
   } catch (error) {
     console.log(error);
   }
