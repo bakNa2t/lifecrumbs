2b2ced586a26436dd2bc100276353f4e05c65c5d
bakNa2t <ok2.lock@gmail.com>
2024-09-13 15:06:31 +0300

    Added SigninValidationSchema

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -10,3 +10,10 @@ export const SignupValidationSchema = z.object({
     .string()
     .min(8, { message: "Password must be at least 8 characters" }),
 });
+
+export const SigninValidationSchema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
