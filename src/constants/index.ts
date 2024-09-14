71d83ae084640db2fe39f1c7ff9ef4d21ade3bdd
bakNa2t <ok2.lock@gmail.com>
2024-09-14 12:51:03 +0300

    Created links constants

---
+++ src/constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinkIcons = [
+  {
+    imgUrl: "assets/icons/sidebar/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgUrl: "assets/icons/sidebar/exploxe.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgUrl: "assets/icons/sidebar/users.svg",
+    route: "/all-users",
+    label: "All Users",
+  },
+  {
+    imgUrl: "assets/icons/sidebar/savemark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgUrl: "assets/icons/sidebar/create.svg",
+    route: "/create-post",
+    label: "Create Post",
+  },
+];
