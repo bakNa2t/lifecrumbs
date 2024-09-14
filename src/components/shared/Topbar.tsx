049500df5f382b6a6f459e428d434820a0fb9c9d
bakNa2t <ok2.lock@gmail.com>
2024-09-14 11:53:25 +0300

    Added Link to profile and signOutAccount fn to Button

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import { Button } from "../ui/button";
+import { useSignOutAccountMutation } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const Topbar = () => {
+  const navigate = useNavigate();
+  const { mutate: signOutAccount, isSuccess } = useSignOutAccountMutation();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess, navigate]);
+
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
@@ -17,7 +29,11 @@ const Topbar = () => {
         </Link>
 
         <div className="flex gap-4">
-          <Button variant="ghost" className="shad-button_ghost">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOutAccount()}
+          >
             <img
               src="/assets/icons/logout.svg"
               width={26}
@@ -25,6 +41,13 @@ const Topbar = () => {
               alt="logout"
             />
           </Button>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "assets/images/profile-default.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
