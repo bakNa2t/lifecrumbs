772e9ff7e7b05ca5c779f70aa1b23cf3244f29bc
bakNa2t <ok2.lock@gmail.com>
2024-09-16 14:29:21 +0300

    Added bottombarLinkIcons

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,3 +25,26 @@ export const sidebarLinkIcons = [
     label: "Create Post",
   },
 ];
+
+export const bottombarLinkIcons = [
+  {
+    imgURL: "assets/icons/sidebar/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "assets/icons/sidebar/explore.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "assets/icons/sidebar/savemark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "assets/icons/sidebar/create.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+];
