2bae8a7fe971a67c85511d7d80039b87ae6e7959
bakNa2t <ok2.lock@gmail.com>
2024-09-16 14:31:30 +0300

    Added and styled Link for bottombar icons

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,5 +1,36 @@
+import { bottombarLinkIcons } from "@/constants";
+
+import { Link, useLocation } from "react-router-dom";
+
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <section className="bottom-bar">
+      {bottombarLinkIcons.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link
+            to={link.route}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px] "
+            } flex-center flex-col gap-1 p-2 transition`}
+            key={link.label}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={`${isActive && "invert-white"}`}
+              width={20}
+              height={20}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
