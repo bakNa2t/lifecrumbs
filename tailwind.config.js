ae5c53b06cf2d0883b9b025f83ea307997800935
bakNa2t <ok2.lock@gmail.com>
2024-09-18 09:48:43 +0300

    Updated extend by adding translate

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,7 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      translate: ["active"],
     },
   },
   plugins: [require("tailwindcss-animate")],
