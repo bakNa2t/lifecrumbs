b8507c7f84f2cf0ba2915126aada1a9110bd6f57
bakNa2t <ok2.lock@gmail.com>
2024-09-23 17:21:55 +0300

    Added Explore

---
+++ src/_root/pages/Explore.tsx
@@ -0,0 +1,19 @@
+const Explore = () => {
+  return (
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold 2-full">Search Post</h2>
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img
+            src="assets/icons/search.svg"
+            alt="search"
+            width={24}
+            height={24}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
