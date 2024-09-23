3ba8f971aa6a977c7615a092bf76b3a3512c12ed
bakNa2t <ok2.lock@gmail.com>
2024-09-23 15:33:34 +0300

    Added Link to edit post

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -18,28 +18,45 @@ const PostDetails = () => {
           <img src={post?.imageUrl} alt="post" className="post_details-img" />
 
           <div className="post_details-info">
-            <Link to={`/profile/${post?.creator.$id}`}>
-              <img
-                src={
-                  post?.creator?.imageUrl || "assets/icons/profile-default.svg"
-                }
-                alt="creator"
-                className="w-12 lg:h-12 rounded-full"
-              />
-            </Link>
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3"
+              >
+                <img
+                  src={
+                    post?.creator?.imageUrl ||
+                    "assets/icons/profile-default.svg"
+                  }
+                  alt="creator"
+                  className="w-12 lg:h-12 rounded-full"
+                />
 
-            <div className="flex flex-col">
-              <p className="base-medium lg:body-bold text-light-1">
-                {post?.creator.name}
-              </p>
-              <div className="flex-center gap-2 text-light-3">
-                <p className="subtle-semibold lg:small-regular">
-                  {formatDate(post?.$createdAt ?? "")}
-                </p>
-                •
-                <p className="subtle-semibold lg:small-regular">
-                  {post?.location}
-                </p>
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular">
+                      {formatDate(post?.$createdAt ?? "")}
+                    </p>
+                    •
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex-center gap-5">
+                <Link to={`/update-post/${post?.$id}`}>
+                  <img
+                    src="/assets/icons/edit.svg"
+                    alt="edit"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
               </div>
             </div>
           </div>
