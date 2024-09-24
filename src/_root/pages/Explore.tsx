5d31dc082b85941e09603be14afe90ebaf7c303a
bakNa2t <ok2.lock@gmail.com>
2024-09-24 10:35:10 +0300

    Added search Input

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,4 +1,9 @@
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
 const Explore = () => {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
@@ -9,6 +14,13 @@ const Explore = () => {
             alt="search"
             width={24}
             height={24}
+          />
+          <Input
+            type="text"
+            placeholder="Search post"
+            className="explore-search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
         </div>
       </div>
