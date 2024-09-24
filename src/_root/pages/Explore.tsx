33ffa0bddc36ae7aacec00c628b5c94e500bcfd5
bakNa2t <ok2.lock@gmail.com>
2024-09-24 10:56:15 +0300

    Added search and filter icons

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -7,7 +7,7 @@ const Explore = () => {
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className="h3-bold md:h2-bold 2-full">Search Post</h2>
+        <h2 className="h3-bold md:h2-bold w-full">Search Post</h2>
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
           <img
             src="assets/icons/search.svg"
@@ -21,6 +21,20 @@ const Explore = () => {
             className="explore-search"
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+        <h3 className="body-bold md:h3-bold ">Last Popular Posts</h3>
+
+        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+          <p className="small-medium md:base-medium text-light-3">All</p>
+          <img
+            src="/assets/icons/filter.svg"
+            alt="filter"
+            width={20}
+            height={20}
           />
         </div>
       </div>
