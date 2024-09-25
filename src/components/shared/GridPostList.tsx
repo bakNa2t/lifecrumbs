82ba7dd9f32c4ff298c33b1249e6c5090da2b263
bakNa2t <ok2.lock@gmail.com>
2024-09-25 17:05:58 +0300

    Added user info and post stat

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -2,12 +2,19 @@ import { Link } from "react-router-dom";
 import { Models } from "appwrite";
 
 import { useUserContext } from "@/context/AuthContext";
+import PostStats from "./PostStats";
 
 type GridPostListProps = {
   posts: Models.Document[];
+  showUser?: boolean;
+  showStats?: boolean;
 };
 
-const GridPostList = ({ posts }: GridPostListProps) => {
+const GridPostList = ({
+  posts,
+  showUser = true,
+  showStats = true,
+}: GridPostListProps) => {
   const { user } = useUserContext();
 
   return (
@@ -21,6 +28,20 @@ const GridPostList = ({ posts }: GridPostListProps) => {
               className="w-full h-full object-cover"
             />
           </Link>
+
+          <div className="grid-post_user">
+            {showUser && (
+              <div className="flex items-center justify-start gap-2 flex-1">
+                <img
+                  src={post.creator.imageUrl}
+                  alt="creator"
+                  className="w-8 h-8 rounded-full"
+                />
+                <p className="line-clamp-1">{post.creator.name}</p>
+              </div>
+            )}
+            {showStats && <PostStats post={post} userId={user.id} />}
+          </div>
         </li>
       ))}
     </ul>
