577ab965f54d68b26defc43372cd18f1d4ac8213
bakNa2t <ok2.lock@gmail.com>
2024-09-25 16:32:57 +0300

    Added useGetPostQuery and useSearchPostQuery fn

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,37 @@
-import { Input } from "@/components/ui/input";
 import { useState } from "react";
+
+import { Input } from "@/components/ui/input";
+import SearchResults from "@/components/shared/SearchResults";
+import GridPostList from "@/components/shared/GridPostList";
+
+import {
+  useGetPostsQuery,
+  useSearchPostsQuery,
+} from "@/lib/react-query/queriesAndMutations";
+import useDebounce from "@/hooks/useDebounce";
+import Loader from "@/components/shared/Loader";
 
 const Explore = () => {
   const [searchValue, setSearchValue] = useState("");
+
+  const { data: posts /*fetchNextPage, hasNexPage*/ } = useGetPostsQuery();
+
+  const debouncedSearchValue = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } =
+    useSearchPostsQuery(debouncedSearchValue);
+
+  if (!posts) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+  }
+
+  const shouldDisplaySearchResults = searchValue !== "";
+  const shouldDisplayPosts =
+    !shouldDisplaySearchResults &&
+    posts?.pages.every((item) => item.documents.length === 0);
 
   return (
     <div className="explore-container">
@@ -29,7 +58,7 @@ const Explore = () => {
         <h3 className="body-bold md:h3-bold ">Last Popular Posts</h3>
 
         <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          <p className="small-medium md:base-medium text-light-3">All</p>
+          <p className="small-medium md:base-medium text-light-2">All</p>
           <img
             src="/assets/icons/filter.svg"
             alt="filter"
@@ -37,6 +66,20 @@ const Explore = () => {
             height={20}
           />
         </div>
+      </div>
+
+      <div className="flex flex-wrap gap-6 w-full max-w-5xl">
+        {shouldDisplaySearchResults ? (
+          <SearchResults />
+        ) : shouldDisplayPosts ? (
+          <p className="text-light-4 mt-10 text-center w-full">
+            No posts found
+          </p>
+        ) : (
+          posts.pages.map((item, index) => (
+            <GridPostList key={`page-${index}`} posts={item.documents} />
+          ))
+        )}
       </div>
     </div>
   );
