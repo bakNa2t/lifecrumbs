b4e54a08c8a9bacb42396144bf7e75f0b7045f01
bakNa2t <ok2.lock@gmail.com>
2024-09-25 17:32:45 +0300

    Added SearchResults

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,25 @@
+import { Models } from "appwrite";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
+
+type searchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
+};
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: searchResultsProps) => {
+  if (isSearchFetching) return <Loader />;
+
+  if (searchedPosts && searchedPosts.documents.length === 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  }
+
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No posts found</p>
+  );
+};
+
+export default SearchResults;
