a706142719466de2b8cb600afae05056f3eff7d7
bakNa2t <ok2.lock@gmail.com>
2024-09-27 12:16:12 +0300

    Created UserCard

---
+++ src/components/shared/UserCard.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { Models } from "appwrite";
+
+import { Button } from "../ui/button";
+
+type UserCardProps = {
+  user: Models.Document;
+};
+
+const UserCard = ({ user }: UserCardProps) => {
+  return (
+    <Link to={`/profile/${user.$id}`} className="user-card">
+      <img
+        src={user.imageUrl || "/assets/icons/profile-default.svg"}
+        alt="creator"
+        className="rounded-full w-12 h-12"
+      />
+
+      <div className="flex-center flex-col gap-1">
+        <p className="base-medium text-light-1 text-center line-clamp-1">
+          {user.name}
+        </p>
+        <p className="small-regular text-light-3 text-center line-clamp-1">
+          @{user.username}
+        </p>
+      </div>
+
+      <Button type="button" size="sm" className="shad-button_primary px-5">
+        Follow
+      </Button>
+    </Link>
+  );
+};
+
+export default UserCard;
