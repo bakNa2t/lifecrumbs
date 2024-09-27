f70431da0bf5cd610204c9a211b2f71a801653c8
bakNa2t <ok2.lock@gmail.com>
2024-09-27 11:02:20 +0300

    Added AllUsers

---
+++ src/_root/pages/AllUsers.tsx
@@ -0,0 +1,11 @@
+const AllUsers = () => {
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All users</h2>
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
