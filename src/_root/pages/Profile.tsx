659ed381db19acd30f79a7debc62429f19071a93
bakNa2t <ok2.lock@gmail.com>
2024-09-30 13:30:37 +0300

    Added StatBlock with StatBlock interface

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -5,6 +5,20 @@ import Loader from "@/components/shared/Loader";
 import { useUserContext } from "@/context/AuthContext";
 import { useGetUserByIdQuery } from "@/lib/react-query/queriesAndMutations";
 
+interface StatBlockProp {
+  value: string | number;
+  label: string;
+}
+
+const StatBlock = ({ value, label }: StatBlockProp) => {
+  return (
+    <div className="flex-center gap-2">
+      <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
+      <p className="small-medium lg:base-medium text-light-2">{label}</p>
+    </div>
+  );
+};
+
 const Profile = () => {
   const { id } = useParams();
   const { user } = useUserContext();
