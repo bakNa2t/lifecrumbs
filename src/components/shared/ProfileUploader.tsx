689ac847be1758b001d51305cccc733b7e84f9ac
bakNa2t <ok2.lock@gmail.com>
2024-10-01 14:58:23 +0300

    Added ProfileUploader

---
+++ src/components/shared/ProfileUploader.tsx
@@ -0,0 +1,37 @@
+import { convertFileToUrl } from "@/lib/utils";
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+
+type ProfileUploaderProps = {
+  fieldChange: (files: File[]) => void;
+  mediaUrl: string;
+};
+
+const ProfileUploader = ({ fieldChange, mediaUrl }: ProfileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(convertFileToUrl(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpg", ".jpeg", ".svg"],
+    },
+  });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} className="cursor-pointer" />
+    </div>
+  );
+};
+
+export default ProfileUploader;
