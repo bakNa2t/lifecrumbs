fa499160f9dbeb4c84bfd0c38c075cd6e19923fc
bakNa2t <ok2.lock@gmail.com>
2024-10-01 12:07:20 +0300

    Added ProfileValidationSchema

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -18,6 +18,16 @@ export const SigninValidationSchema = z.object({
     .min(8, { message: "Password must be at least 8 characters" }),
 });
 
+export const ProfileValidatinSchema = z.object({
+  file: z.custom<File[]>(),
+  name: z.string().min(2, { message: "Name must be at least 2 characters" }),
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters" }),
+  email: z.string().email(),
+  bio: z.string().min(5).max(2200),
+});
+
 export const PostValidationSchema = z.object({
   caption: z.string().min(5).max(2200),
   file: z.custom<File[]>(),
