e9ad169edf606b64747a9e7f16da71241c33f734
bakNa2t <ok2.lock@gmail.com>
2024-10-01 14:43:28 +0300

    Added convertFileToUrl fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -29,3 +29,5 @@ export function formatDate(dateStr: string): string {
 export const checkIsLiked = (likeList: string[], userId: string) => {
   return likeList.includes(userId);
 };
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
