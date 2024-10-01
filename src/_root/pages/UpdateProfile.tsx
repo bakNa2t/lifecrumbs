16d39e12bf6780933dc2951ac1a2a6c092dfdb83
bakNa2t <ok2.lock@gmail.com>
2024-10-01 15:41:48 +0300

    Added Form to dispaly

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -1,13 +1,25 @@
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import Loader from "@/components/shared/Loader";
+import ProfileUploader from "@/components/shared/ProfileUploader";
 
 import { useToast } from "@/hooks/use-toast";
 import { useUserContext } from "@/context/AuthContext";
 import { ProfileValidatinSchema } from "@/lib/validation";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   useGetUserByIdQuery,
   useUpdateUserMutation,
@@ -45,15 +57,118 @@ const UpdateProfile = () => {
     <div className="flex flex-1">
       <div className="common-container">
         <div className="flex-start gap-3 justify-start w-full max-w-5xl">
-          <img
-            src="/assets/icons/edit.svg"
-            alt="edit"
-            className="invert-white"
-            width={32}
-            height={32}
-          />
+          <img src="/assets/icons/edit.svg" alt="edit" width={32} height={32} />
           <h2 className="h3-bold md:h2-bold text-left w-full">Edit profile</h2>
         </div>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(() => {})}
+            className="flex flex-col gap-9 w-full max-w-5xl"
+          >
+            <FormField
+              control={form.control}
+              name="file"
+              render={({ field }) => (
+                <FormItem className="flex">
+                  <FormControl>
+                    <ProfileUploader
+                      fieldChange={field.onChange}
+                      mediaUrl={currentUser.imageUrl}
+                    />
+                  </FormControl>
+                  <FormMessage className="shard-form_message" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="shard-form_label">Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" className="shad-input" {...field} />
+                  </FormControl>
+                  <FormMessage className="shard-form_message" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="shard-form_label">Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="shad-input"
+                      {...field}
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage className="shard-form_message" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="shard-form_label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      className="shad-input"
+                      {...field}
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage className="shard-form_message" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="bio"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="shard-form_label">Bio</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="shad-textarea custom-scrollbar"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="shard-form_message" />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex gap-4 items-center justify-end">
+              <Button
+                type="button"
+                className="shad-button_dark_4"
+                onClick={() => navigate(-1)}
+              >
+                Cancel
+              </Button>
+
+              <Button
+                type="submit"
+                className="shad-button_primary whitespace-nowrap active:translate-y-[2px]"
+              >
+                Update Profile
+              </Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </div>
   );
