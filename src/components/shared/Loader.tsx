43ccf10046622b4030fba4f7b4d4b77f97d37394
bakNa2t <ok2.lock@gmail.com>
2024-10-07 13:35:52 +0300

    Added props for width and height

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,7 +1,12 @@
-const Loader = () => {
+const Loader = ({ wdth, hgt }: { wdth: number; hgt: number }) => {
   return (
     <div className="flex-center w-full">
-      <img src="/assets/icons/loader.svg" alt="Loader" width={24} height={24} />
+      <img
+        src="/assets/icons/loader.svg"
+        alt="Loader"
+        width={wdth}
+        height={hgt}
+      />
     </div>
   );
 };
