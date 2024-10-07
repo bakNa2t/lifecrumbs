72f952f5d72881ccdc2ab2c288d79f6fc4e4c272
bakNa2t <ok2.lock@gmail.com>
2024-10-07 14:06:09 +0300

    Added width/height var to export

--- src/hooks/useMobileScreen.ts
+++ src/hooks/useMobileScreen.ts
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 
 export default function useMobileScreen() {
-  const [isMobileScreen, setIsMobileScreen] = useState(false);
+  const [isMobileScreen, setIsMobileScreen] = useState(screen.width <= 768);
 
   useEffect(() => {
-    if (window.innerWidth <= 768) {
+    if (screen.width <= 768) {
       setIsMobileScreen(true);
     } else {
       setIsMobileScreen(false);
     }
-  }, [isMobileScreen]);
+  }, []);
 
-  return { isMobileScreen };
+  let wdth, hgt;
+  if (isMobileScreen) {
+    wdth = 24;
+    hgt = 24;
+  } else {
+    wdth = 42;
+    hgt = 42;
+  }
+
+  return { isMobileScreen, wdth, hgt };
 }
