56e81138d89d65842c9153f17288d320ad8a55cd
bakNa2t <ok2.lock@gmail.com>
2024-10-15 14:36:49 +0300

    Updated SelectBlock style

--- src/components/shared/SelectBlock.tsx
+++ src/components/shared/SelectBlock.tsx
@@ -13,18 +13,20 @@ type SelectBlockProps = {
 const SelectBlock = ({ options }: SelectBlockProps) => {
   return (
     <Select>
-      <SelectTrigger className="max-w-[135px] flex gap-1">
+      <SelectTrigger className="max-w-[135px] flex gap-1 border-dark-4 dark:border-bright-4">
         <SelectValue placeholder="Sort by date" />
       </SelectTrigger>
-      <SelectContent className="bg-dark-3">
+      <SelectContent className="bg-dark-3 dark:bg-bright-3">
         {options.map(({ value, label, path }) => (
           <SelectItem
             key={value}
             value={value}
-            className="cursor-pointer hover:bg-dark-4 rounded-md"
+            className="cursor-pointer hover:bg-dark-4 dark:hover:bg-bright-4 rounded-md"
           >
             <div className="flex gap-2">
-              <p className="uppercase test-sm md:text-[12px]">{label}</p>
+              <p className="uppercase test-sm md:text-[12px] font-medium">
+                {label}
+              </p>
               <img src={path} alt="filter" width={14} height={14} />
             </div>
           </SelectItem>
