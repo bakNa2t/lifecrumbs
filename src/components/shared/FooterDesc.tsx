54f9bed67e76f58d9c4a1913873310ae582cb283
bakNa2t <ok2.lock@gmail.com>
2024-10-19 12:39:15 +0300

    Added styles to FooterDesc

--- src/components/shared/FooterDesc.tsx
+++ src/components/shared/FooterDesc.tsx
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom";
 
 const FooterDesc = () => {
   return (
-    <div>
-      Copyright {new Date().getFullYear()}. Design by{" "}
+    <div className="absolute translate-x-1/2 bottom-2 flex-center tiny-medium text-primary-500 gap-1">
+      Copyright {new Date().getFullYear()}. Design by
       <Link to="https://github..com/bakna2t" target="_blank" rel="noreferrer">
         _&#216;k
       </Link>
-      .
     </div>
   );
 };
