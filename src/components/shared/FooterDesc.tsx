acca0117d6d7042f8566ad42ca23d495bf08ddd2
bakNa2t <ok2.lock@gmail.com>
2024-10-20 16:59:37 +0300

    Updated transform Link style by hovering

--- src/components/shared/FooterDesc.tsx
+++ src/components/shared/FooterDesc.tsx
@@ -4,7 +4,12 @@ const FooterDesc = () => {
   return (
     <div className="absolute w-full left-1/2 transform -translate-x-1/2 bottom-2 flex-center tiny-medium text-primary-500 gap-1">
       Copyright {new Date().getFullYear()}. Design by
-      <Link to="https://github..com/bakna2t" target="_blank" rel="noreferrer">
+      <Link
+        to="https://github..com/bakna2t"
+        target="_blank"
+        rel="noreferrer"
+        className="hover:transform hover:-translate-y-1 hover:text-light-4 duration-300"
+      >
         _&#216;k
       </Link>
     </div>
