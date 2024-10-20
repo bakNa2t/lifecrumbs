020ef8fd81b39bb1ceda3f468cae48592b88f202
bakNa2t <ok2.lock@gmail.com>
2024-10-20 16:47:06 +0300

    Updated position cls

--- src/components/shared/FooterDesc.tsx
+++ src/components/shared/FooterDesc.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const FooterDesc = () => {
   return (
-    <div className="absolute translate-x-1/2 bottom-2 flex-center tiny-medium text-primary-500 gap-1">
+    <div className="absolute w-full left-1/2 transform -translate-x-1/2 bottom-2 flex-center tiny-medium text-primary-500 gap-1">
       Copyright {new Date().getFullYear()}. Design by
       <Link to="https://github..com/bakna2t" target="_blank" rel="noreferrer">
         _&#216;k
