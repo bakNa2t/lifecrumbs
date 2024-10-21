a0b5b9f3733d5b569d0bf4fb9d2a20c21325b7f3
bakNa2t <ok2.lock@gmail.com>
2024-10-21 11:06:47 +0300

    Added dark cls and hover drop-shadow to Link

--- src/components/shared/FooterDesc.tsx
+++ src/components/shared/FooterDesc.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const FooterDesc = () => {
   return (
-    <div className="absolute w-full left-1/2 transform -translate-x-1/2 bottom-2 flex-center tiny-medium text-primary-500 gap-1">
+    <div className="absolute w-full left-1/2 transform -translate-x-1/2 bottom-2 flex-center tiny-medium text-dark-4 dark:text-light-4  gap-1">
       Copyright {new Date().getFullYear()}. Design by
       <Link
-        to="https://github..com/bakna2t"
+        to="https://github.com/bakna2t"
         target="_blank"
         rel="noreferrer"
-        className="hover:transform hover:-translate-y-1 hover:text-light-4 duration-300"
+        className="hover:transform hover:-translate-y-1 hover:text-bright-4 dark:hover:text-light-1 duration-300 hover:drop-shadow-dusk_shadow dark:hover:drop-shadow-light_shadow"
       >
         _&#216;k
       </Link>
