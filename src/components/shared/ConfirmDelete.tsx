7347f81616ed5f0f050abe87b2a703746a44f7f5
bakNa2t <ok2.lock@gmail.com>
2024-10-23 14:55:20 +0300

    Updated styles for dark theme

--- src/components/shared/ConfirmDelete.tsx
+++ src/components/shared/ConfirmDelete.tsx
@@ -10,9 +10,9 @@ const ConfirmDelete = ({
   handleDeletePost,
 }: ConfirmDeleteProps) => {
   return (
-    <div className="fixed inset-0 bg-dark-1 bg-opacity-75 transition-opacity flex flex-col items-center justify-center z-100">
-      <div className="inline-block align-bottom bg-dark-4 rounded-lg text-left overflow-hidden shadow-lg shadow-primary-500/60 sm:shadow-xl sm:shadow-primary-500/60 transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-        <div className="bg-dark-2 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+    <div className="fixed backdrop-filter backdrop-blur-sm inset-0 bg-bright-4 dark:bg-dark-1 bg-opacity-60 dark:bg-opacity-60 transition-opacity flex flex-col items-center justify-center z-[100]">
+      <div className="inline-block align-bottom bg-bright-4 dark:bg-dark-4 rounded-lg text-left overflow-hidden shadow-lg shadow-bright-4/60 dark:shadow-primary-500/60 sm:shadow-xl sm:shadow-bright-4/60 dark:sm:shadow-primary-500/60 transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div className="bg-bright-2 dark:bg-dark-2 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
               <svg
@@ -33,11 +33,11 @@ const ConfirmDelete = ({
             </div>
 
             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <h3 className="text-lg leading-6 font-medium text-light-3">
+              <h3 className="text-lg leading-6 font-medium text-dark-1 dark:text-light-3">
                 Delete Post?
               </h3>
               <div className="mt-2">
-                <p className="text-sm text-light-4">
+                <p className="text-sm text-dark-3 dark:text-light-4">
                   Are you sure you want to delete this post?
                 </p>
               </div>
@@ -45,7 +45,7 @@ const ConfirmDelete = ({
           </div>
         </div>
 
-        <div className="bg-dark-3 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse sm:gap-2">
+        <div className="bg-bright-3 dark:bg-dark-3 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse sm:gap-2">
           <Button
             type="button"
             className="shad-button_primary w-full sm:w-auto mb-3 sm:mb-0 active:translate-y-[-2px] hover:shadow-lg hover:shadow-light-3"
