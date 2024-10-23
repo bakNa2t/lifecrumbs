a1a797a5370f114f9064013aeeb659ff6af13991
bakNa2t <ok2.lock@gmail.com>
2024-10-23 10:49:43 +0300

    Added copyright sign

--- src/components/shared/FooterDesc.tsx
+++ src/components/shared/FooterDesc.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const FooterDesc = () => {
   return (
     <div className="absolute w-full left-1/2 transform -translate-x-1/2 bottom-2 flex-center tiny-medium text-dark-4 dark:text-light-4  gap-1">
-      Copyright {new Date().getFullYear()}. Design by
+      &copy; Copyright {new Date().getFullYear()}. Design by
       <Link
         to="https://github.com/bakna2t"
         target="_blank"
