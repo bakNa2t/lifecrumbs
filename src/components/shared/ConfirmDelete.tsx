b4d65068c440359b862a3eb1fa935f52f75ffdbd
bakNa2t <ok2.lock@gmail.com>
2024-10-23 10:54:50 +0300

    Added ConfirmDeleteProps and passed handleCancelDelete, handleDisplayConfirmDelete to certain btns

--- src/components/shared/ConfirmDelete.tsx
+++ src/components/shared/ConfirmDelete.tsx
@@ -1,4 +1,12 @@
-const ConfirmDelete = () => {
+type ConfirmDeleteProps = {
+  handleCancelDelete: () => void;
+  handleDeletePost: () => void;
+};
+
+const ConfirmDelete = ({
+  handleCancelDelete,
+  handleDeletePost,
+}: ConfirmDeleteProps) => {
   return (
     <div
       className="fixed inset-0 bg-dark-1 bg-opacity-75 transition-opacity flex flex-col items-center justify-center"
@@ -40,18 +48,18 @@ const ConfirmDelete = () => {
           </div>
         </div>
 
-        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <div className="bg-dark-2 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <button
             type="button"
             className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-            //   onClick={handleConfirmDelete}
+            onClick={handleDeletePost}
           >
             Confirm
           </button>
           <button
             type="button"
             className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-            //   onClick={handleCancelDelete}
+            onClick={handleCancelDelete}
           >
             Cancel
           </button>
